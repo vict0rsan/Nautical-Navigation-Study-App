4557b1d5a0038fd4e2605fbe887c1c1169d13733
vict0rsan <vicloligye@hotmail.com>
2022-05-04 17:52:39 +0200

    Ya pirula loco

--- POI_UPV/src/poiupv/PoiUPVApp.java
+++ POI_UPV/src/poiupv/PoiUPVApp.java
@@ -1,9 +1,11 @@
+package poiupv;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package poiupv.application;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +21,7 @@ public class PoiUPVApp extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLSignUp.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/poiupv/view/Welcome.fxml"));
         
         Scene scene = new Scene(root);
         stage.setTitle("Puntos de interes UPV");
