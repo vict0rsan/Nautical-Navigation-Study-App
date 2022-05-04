9f9d728c65584194a743ff359d4cdc7dc53cb2d2
vict0rsan <vicloligye@hotmail.com>
2022-05-04 17:17:23 +0200

    First commit

---
+++ POI_UPV/src/poiupv/controller/FXMLSignUpController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package poiupv.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author VicLo
+ */
+public class FXMLSignUpController implements Initializable {
+
+    @FXML
+    private Button bAccept;
+    @FXML
+    private Button bCancel;
+    @FXML
+    private TextField email;
+    @FXML
+    private Label lIncorrectEmail;
+    @FXML
+    private PasswordField pass;
+    @FXML
+    private Label lIncorrectPass;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleBAcceptOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleBCancelOnAction(ActionEvent event) {
+    }
+    
+}
---
+++ POI_UPV/src/poiupv/controller/WelcomeController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poiupv.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author VicLo
+ */
+public class WelcomeController {
+
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private void handleLoginButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleSignUpButton(ActionEvent event) {
+    }
+    
+}
