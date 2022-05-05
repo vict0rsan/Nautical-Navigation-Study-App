cc733356dbfcbd8a52ab20fc31d8a3530bc5d21a
d34dHusky <d34dHusky@users.noreply.github.com>
2022-05-05 13:33:02 +0200

    Avatar Selector pito

---
+++ POI_UPV/src/poiupv/controller/AvatarSelectorController.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poiupv.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author guill
+ */
+public class AvatarSelectorController implements Initializable {
+
+    @FXML
+    private Button avatar1Button;
+    @FXML
+    private ImageView avatar1;
+    @FXML
+    private Button avatar2Button;
+    @FXML
+    private ImageView avatar2;
+    @FXML
+    private Button avatar3Button;
+    @FXML
+    private ImageView avatar3;
+    @FXML
+    private Button avatar4Button;
+    @FXML
+    private ImageView avatar4;
+    @FXML
+    private Button avatarDefaultButton;
+    @FXML
+    private ImageView avatarDefault;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleAvatar1(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAvatar2(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAvatar3(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAvatar4(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleAvatarDefault(ActionEvent event) {
+    }
+    
+}
